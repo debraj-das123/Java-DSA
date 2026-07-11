d0a2f3b6447bd8fb01109afca59fc5f6335bd09f
Debraj Das <debrajdas.dgr@gmail.com>
2026-07-11 19:14:09 +0600

    ADD Numers of vowelse

---
+++ string/NoVowels.java
@@ -0,0 +1,20 @@
+package string;
+
+public class NoVowels {
+        public static void main(String [] args){
+        String str = "Diuygtyygtabsrtfuytfdrhg";
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+           if(c =='a' || c =='e' || c =='i'|| c =='o'|| c =='u'  ){
+            count++;
+
+           }
+            
+        }
+
+        System.out.println("total no of Vowels:"  + count);
+
+        System.out.println(str.length());
+    }
+}
